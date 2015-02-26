4650dba833d6cfd203f29f967d84aae94f3e8563
Annie Lydens <aelydens@gmail.com>
2015-02-25 21:57:20 -0700

    Add first pass at user_name method to talks model

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -26,4 +26,9 @@ class Talk < ActiveRecord::Base
       self.errors[:base] << "There are already 5 lightning talks for that day."
     end
   end
+
+  def user_name
+    user = User.where(id: self.user_id)
+    user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   get "sign-up", to: "users#new"
   post "sign-up", to: "users#create"
   delete "sign-out", to: "sessions#destroy"
-  get "sign-in", to: "sessions#new"
-  post "sign-in", to: "sessions#create"
   get "/auth/:provider/callback" => "sessions#create"
   get "/signout" => "sessions#destroy", :as => :signout
   resources :users do
